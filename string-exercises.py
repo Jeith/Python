fb52683d4ea8e999daa2d55a68e8cf96e9e2419c
Jeith <37682391+Jeith@users.noreply.github.com>
2018-06-28 09:22:11 -0500

    updating finished assignment

--- string-exercises.py
+++ string-exercises.py
@@ -31,18 +31,34 @@ print("----------------------")
 print("-----Challenge 4------")
 print("----------------------")
 print("")
-
+myString = "Given a paragraph of text as a string, print the paragraph in leetspeak. To translate a string to leetspeak, you need to replace make the following character replacements"
+for char in myString:
+	if char == 'a':
+		myString = myString.replace('a','4')
+	elif char == 'i':
+		myString = myString.replace('i','1')
+	elif char == 'e':
+		myString = myString.replace('e','3')
+	elif char == 'g':
+		myString = myString.replace('g','9')
+	elif char == 'o':
+		myString = myString.replace('o','0')
+	elif char == 's': 
+		myString = myString.replace('s','5')
+	elif char == 't':
+		myString = myString.replace('t','7')
+print(myString)
 
 print("")
 print("----------------------")
 print("-----Challenge 5------")
 print("----------------------")
 print("")
-i = 0
-j = 1
 myString = "Man Spoon"
-while j < len(myString):
-    if myString[i] == myString[j]:
+myString = myString.replace('ee', 'eeeee')
+myString = myString.replace('oo', 'ooooo')
+myString = myString.replace("aa", "aaaaa")
+print(word)
         
 
 print("")
@@ -50,21 +66,15 @@ print("----------------------")
 print("-----Challenge 6------")
 print("----------------------")
 print("")
-
-print("")
-print("----------------------")
-print("-----Challenge 7------")
-print("----------------------")
-print("")
-
-print("")
-print("----------------------")
-print("-----Challenge 8------")
-print("----------------------")
-print("")
-
-print("")
-print("----------------------")
-print("-----Challenge 9------")
-print("----------------------")
-print("")
+cipher = "lbh zhfg hayrnea jung lbh unir yrnearq"
+alphabet = ["a", "b", "c", "d", "e", "f","g", "h","i","j","k", "l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+position = 0
+newPosition = 0
+decodedPosition = 0
+for cipherLetter in cipher:
+    if cipherLetter.isspace() == False :
+        position = alphabet.index(cipherLetter)
+        newPosition = position - 13
+        if newPosition < 0:
+            newPosition = 26 + newPosition
+        print(alphabet[newPosition])
